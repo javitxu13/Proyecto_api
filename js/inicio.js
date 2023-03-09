21bc50c631860abcf7301cd13bb496dcb92ae667
Javier <javiergonzalez13a@gmail.com>
2023-03-09 20:32:20 +0100

    Funcion Borrar

--- js/inicio.js
+++ js/inicio.js
@@ -3,11 +3,13 @@ document.getElementById("artist").addEventListener("click", search);
 document.getElementById("album").addEventListener("click", search);
 
 function search(event) {
-  let text = document.getElementById("Buscar").value;
+  let text = document.getElementById("Buscar").value.trim();
+  if(text === "") return;
   let textEncoded = encodeURIComponent(text);
   let sectionResult = document.getElementById("Resultados");
   let boton = event.target;
   let type = boton.id;
+
   fetch(`https://api.spotify.com/v1/search?q=${textEncoded}&type=${type}&`, {
     method: "GET",
     headers: {
@@ -16,6 +18,7 @@ function search(event) {
   })
     .then((response) => response.json())
     .then((data) => {
+      borrarLista();
       if (type === "track") {
         let items = data.tracks.items;
         showTracks(items);
@@ -51,6 +54,14 @@ function showAlbums(album) {
   });
 }
 
+
+function borrarLista(){
+  let miDiv = document.getElementById("miLista");
+  miDiv.innerHTML = "";
+}
+
+
+
 function mostrarElemento(elemento) {
   // Obtenemos la lista del HTML y la asignamos a una variable
   let lista = document.getElementById('miLista');
@@ -59,8 +70,7 @@ function mostrarElemento(elemento) {
   let nuevoElemento = document.createElement('li');
   nuevoElemento.textContent = elemento;
 
-  //li.addEventListener("keyup",searchAlbums); /*cada vez q se crea un nuevo elemento de la lista, le estamos pasando la función clickImportant para que podamos*/
-
   // Agregamos el nuevo elemento a la lista
   lista.appendChild(nuevoElemento);
 }
+
