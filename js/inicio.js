16f3c5c3681159b6a1c1f76e9573c76688cf710f
Javier <javiergonzalez13a@gmail.com>
2023-03-10 13:37:30 +0100

    Añadir funciones Fav

--- js/inicio.js
+++ js/inicio.js
@@ -3,11 +3,13 @@ document.getElementById("artist").addEventListener("click", search);
 document.getElementById("album").addEventListener("click", search);
 
 function search(event) {
-  let text = document.getElementById("Buscar").value;
+  let text = document.getElementById("Buscar").value.trim();
+  if(text === "") return;
   let textEncoded = encodeURIComponent(text);
   let sectionResult = document.getElementById("Resultados");
   let boton = event.target;
   let type = boton.id;
+
   fetch(`https://api.spotify.com/v1/search?q=${textEncoded}&type=${type}&`, {
     method: "GET",
     headers: {
@@ -16,6 +18,7 @@ function search(event) {
   })
     .then((response) => response.json())
     .then((data) => {
+      borrarLista();
       if (type === "track") {
         let items = data.tracks.items;
         showTracks(items);
@@ -51,6 +54,13 @@ function showAlbums(album) {
   });
 }
 
+
+function borrarLista(){
+  let miDiv = document.getElementById("miLista");
+  miDiv.innerHTML = "";
+}
+
+
 function mostrarElemento(elemento) {
   // Obtenemos la lista del HTML y la asignamos a una variable
   let lista = document.getElementById('miLista');
@@ -59,8 +69,90 @@ function mostrarElemento(elemento) {
   let nuevoElemento = document.createElement('li');
   nuevoElemento.textContent = elemento;
 
-  //li.addEventListener("keyup",searchAlbums); /*cada vez q se crea un nuevo elemento de la lista, le estamos pasando la función clickImportant para que podamos*/
+  //Creamos icono fav
+  let iconoEstrella = document.createElement("i");
+  iconoEstrella.classList.add("fa", "fa-heart");
+  nuevoElemento.appendChild(iconoEstrella);
 
   // Agregamos el nuevo elemento a la lista
   lista.appendChild(nuevoElemento);
+
+  
 }
+
+
+ function listaFavoritos(){
+  // Obtener el elemento de lista que queremos
+  let lista = document.getElementById("miLista");
+
+  // Crear un botón de favoritos y agregarlo a cada elemento de la lista
+  let elementos = lista.getElementsByTagName("li");
+  for (let i = 0; i < elementos.length; i++) {
+    let botonFavoritos = document.createElement("button");
+    let iconoEstrella = document.createElement("i");
+    botonFavoritos.appendChild(iconoEstrella);
+    elementos[i].appendChild(botonFavoritos);
+
+    // Agregar un manejador de eventos al botón de favoritos
+    botonFavoritos.addEventListener("click",()=>{
+      addFavorito
+    });
+  }
+} 
+
+function getFavoritos(){
+  let favoritos = localStorage.getItem("Favoritos")
+  if(favoritos==null){
+    favoritos = []
+  }else{
+    favoritos = JSON.parse(favoritos)
+  }
+  return favoritos;
+
+}
+
+function addFavorito(nombre){
+  let favoritos = getFavoritos()
+  if(favoritos.includes(nombre)){
+    return;
+  }
+  favoritos.push(nombre)
+  let favoritosString = JSON.stringify(favoritos);
+  localStorage.setItem('Favoritos', favoritosString);
+
+
+  
+}
+
+
+
+/* // Función para agregar elementos a favoritos (ya definida anteriormente)
+function agregarAFavoritos(elemento) {
+  let favoritos = JSON.parse(localStorage.getItem('favoritos')) || [];
+  if (favoritos.some(item => item.id === elemento.id)) {
+    console.log('El elemento ya está en favoritos.');
+    return;
+  }
+  favoritos.push(elemento);
+  localStorage.setItem('favoritos', JSON.stringify(favoritos));
+  console.log('El elemento ha sido agregado a favoritos.');
+}
+
+// Obtenemos el elemento guardado en el local storage
+const elementoDesdeLocalStorage = localStorage.getItem('Articulo añadido a favoritos');
+
+// Convertimos el elemento de texto a su formato original
+const elementoEnFormatoOriginal = JSON.parse(elementoDesdeLocalStorage);
+
+console.log(elementoEnFormatoOriginal); // 'elemento2'
+
+
+
+ */
+
+
+
+
+
+
+
