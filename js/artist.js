d2423517097b6fb53a798b2a6eeec795cb0a0fbb
Anderwander <ander1993trabajo@hotmail.com>
2023-03-11 11:58:28 +0100

    ahora se puede ver el artista, su foto, generos y el link. Añado también algún css y cambio logo

--- js/artist.js
+++ js/artist.js
@@ -18,29 +18,27 @@ console.log(id);
 goToArtist(id);
 
 function createArtist(artist) {
-  let objeto = document.getElementById("miLista");
   let name = artist.name;
-  let images = artist.images;
+  let images = artist.images[0].url;
   let genres = artist.genres;
   let link = artist.external_urls.spotify;
-  console.log(link);
+  let resultado = document.getElementById("results");
   let nombre = document.createElement("h2");
-  nombre.appendChild(name);
-  objeto.appendChild(nombre);
+  let imagen = document.createElement("img");
+  let generos = document.createElement("h3");
+  let enlace = document.createElement("a");
+  nombre.setAttribute("id", "nombreGrupo");
+  imagen.setAttribute("id", "imagenGrupo");
+  imagen.setAttribute("src", images);
+  generos.setAttribute("id", "generoGrupo");
+  enlace.setAttribute("id", "enlaceGrupo");
+  nombre.innerText = name;
+  imagen.innerText = images;
+  generos.innerText = genres;
+  enlace.innerText = link;
+  resultado.appendChild(nombre);
+  resultado.appendChild(imagen);
+  resultado.appendChild(generos);
+  resultado.appendChild(enlace);
+  console.log(images);
 }
-
-// Obtenemos la lista del HTML y la asignamos a una variable
-let lista = document.getElementById("miLista");
-
-// Creamos un elemento <li> y le asignamos el texto pasado como argumento
-let nuevoElemento = document.createElement("li");
-let link = document.createElement("a");
-link.setAttribute("href", "artist.html?id=" + elemento.id);
-link.setAttribute("target", "_blank");
-link.textContent = elemento.name;
-nuevoElemento.appendChild(link);
-
-//li.addEventListener("keyup",searchAlbums); /*cada vez q se crea un nuevo elemento de la lista, le estamos pasando la función clickImportant para que podamos*/
-
-// Agregamos el nuevo elemento a la lista
-lista.appendChild(nuevoElemento);
